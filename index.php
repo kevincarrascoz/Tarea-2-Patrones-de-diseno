aac199bd0128c91a04f38099a8066c0bfc38e1a9
kevincarrascoz <71083755+kevincarrascoz@users.noreply.github.com>
2021-10-02 19:38:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ use models\Efectivo;
 use models\Tarjeta;
 
 
-function echoMostrar($nombre, $objeto, $br = true) {
+function mostrarInfo($nombre, $objeto, $br = true) {
     echo $nombre;
     echo "<br />";
     echo $objeto->mostrar();
@@ -24,50 +24,50 @@ function echoMostrar($nombre, $objeto, $br = true) {
 
 // instanciacion de producto: gelatina
 $productoGelatina = Producto::nuevoProducto("gelatina", 0.50, 250.0, 1000);
-echoMostrar("Producto: Gelatina", $productoGelatina);
+mostrarInfo("Producto: Gelatina", $productoGelatina);
 
 // instanciacion de producto: mermelada
 $productoMermelada = Producto::nuevoProducto("mermelada", 0.5, 500.0, 1000);
-echoMostrar("Producto: Mermelada", $productoMermelada);
+mostrarInfo("Producto: Mermelada", $productoMermelada);
 
 //instanciacion de producto: pan
 $productoPan = Producto::nuevoProducto("pan", 1.0, 100.0, 2500);
-echoMostrar("Producto: Pan", $productoPan);
+mostrarInfo("Producto: Pan", $productoPan);
 
 // instanciacion de cliente
 $nuevoCliente = new Cliente("Antonio Salazar", "Calle Sincera 123");
-echoMostrar("Cliente", $nuevoCliente);
+mostrarInfo("Cliente", $nuevoCliente);
 
 $orden = array (
     "pan" => 10,
     "gelatina" => 5
 );
 $nuevoPedido = $nuevoCliente->solicitar($orden);
-echoMostrar("Pedido de cliente", $nuevoPedido, false);
+mostrarInfo("Pedido de cliente", $nuevoPedido, false);
 
-echo "<ul><li>total a pagar pedido:<br />";
+echo "<li>total a pagar pedido: ";
 echo $nuevoPedido->calcularTotal();
-echo "</li></ul>";
+echo "</li>";
 
 /*instanciación de un nuevo pago con cheque*/ 
 $nuevoCheque = new Cheque(2530,'Cheque1','Banco Estado');
-echoMostrar("Cheque", $nuevoCheque);
+mostrarInfo("Cheque", $nuevoCheque);
 
 /*instanciación de un nuevo pago con efectivo*/ 
 $nuevoEfectivo = new Efectivo(1530,'CLP');
-echoMostrar("Efectivo", $nuevoEfectivo);
+mostrarInfo("Efectivo", $nuevoEfectivo);
 
 /*instanciación de un nuevo pago con tarjeta*/ 
 $nuevoTarjeta = new Tarjeta(530, 241500,'09-15-2020','Mastercard');
-echoMostrar("Tarjeta", $nuevoTarjeta);
+mostrarInfo("Tarjeta", $nuevoTarjeta);
 
 /*autorizacion del pago con cheque */
 $nuevoCheque->autorizar();
-echoMostrar("Cheque", $nuevoCheque);
+mostrarInfo("Cheque", $nuevoCheque);
 
 /*autorizacion del pago con tarjeta */
 $nuevoTarjeta->autorizar();
-echoMostrar("Tarjeta autorizada", $nuevoTarjeta);
+mostrarInfo("Tarjeta autorizada", $nuevoTarjeta);
 
 
 
